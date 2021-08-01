347530cf0810e98b6711daa9b5083331c640dc15
Alejo Gustavo Francomano <alejo@kodacon.com>
2021-08-01 17:12:31 -0300

    refactor lat long

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -7,25 +7,31 @@ import { HttpClient } from '@angular/common/http';
 export class WeatherService {
 
   uri: string = '';
+  varLong: number;
+  varLat: number;
  
 
   constructor(private http: HttpClient) { 
 
-    this.uri = `https://api.openweathermap.org/data/2.5/weather?lat=-34.74206306265408&lon=-58.39864977413688&appid=204a3108bcfcf9c62f8b1342bb34ff39&units=metric`;
+    this.uri = `https://api.openweathermap.org/data/2.5/weather?`;
   }
-  
+
   getPosition(): Promise<any> {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resp => {
                 resolve({lng: resp.coords.longitude, lat: resp.coords.latitude});
+                 this.varLong = resp.coords.longitude;
+                 this.varLat = resp.coords.latitude;
+                 console.log('varLat:', this.varLat , 'varLong:', this.varLong );
             },
             err => {
                 reject(err);
           });
     });
-}
+  }
+
 
   getWeather(){
-    return this.http.get(this.uri);
+    return this.http.get(`${this.uri}lat=${this.varLat}&lon=${this.varLong}&appid=204a3108bcfcf9c62f8b1342bb34ff39&units=metric`);
   }
 }
